8de703701c9d2c4672bb3c44d4604923ac1b5384
dfreeman3102 <davidfreeman3102@gmail.com>
2024-03-06 23:55:39 -0500

    set up for local storage setting and retrieval

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,20 @@ $(function () {
       var timeSelector = $(this).parent().attr("id");
       console.log(timeSelector);
 
+      //places items in local storage
+      localStorage.setItem(timeSelector, inputText);
     })
+      //loads saved data from localStorage in each separate time block
+      $("hour-9 .description").val(localStorage.getItem("hour9"));
+      $("hour-10 .description").val(localStorage.getItem("hour10"));
+      $("hour-11 .description").val(localStorage.getItem("hour11"));
+      $("hour-12 .description").val(localStorage.getItem("hour12"));
+      $("hour-13 .description").val(localStorage.getItem("hour13"));
+      $("hour-14 .description").val(localStorage.getItem("hour14"));
+      $("hour-15 .description").val(localStorage.getItem("hour15"));
+      $("hour-16 .description").val(localStorage.getItem("hour16"));
+      $("hour-17 .description").val(localStorage.getItem("hour17"));
+
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
     // attribute of each time-block be used to conditionally add or remove the
