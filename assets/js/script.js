2c2c60c5bb7d058c2d48bfd62b8c5645006a425e
dfreeman3102 <davidfreeman3102@gmail.com>
2024-03-09 08:46:49 -0500

    added document.ready function to make code run at the end of page loading

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,60 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  //listener for click events on the save button
-  $(".saveBtn").click(function () {
-    //shows the button info in the console
-    console.log(this);
-    //consoles the input text from the text areas with the class of description
-    var inputText = $(this).siblings(".description").val();
-    console.log(inputText);
+$(document).ready(function () {
+  $(function () {
+    //listener for click events on the save button
+    $(".saveBtn").click(function () {
+      //shows the button info in the console
+      console.log(this);
+      //consoles the input text from the text areas with the class of description
+      var inputText = $(this).siblings(".description").val();
+      console.log(inputText);
 
-    //consoles the id from the original div element containing the hour-x
-    var timeSelector = $(this).parent().attr("id");
-    console.log(timeSelector);
+      //consoles the id from the original div element containing the hour-x
+      var timeSelector = $(this).parent().attr("id");
+      console.log(timeSelector);
 
-    // places items in local storage
-    localStorage.setItem(timeSelector, inputText); 
-  }) 
-  // user input saved in localStorage set to the values of the individual textarea elements.
-  $("#hour-9 .description").text(localStorage.getItem("hour-9"));
-  $("#hour-10 .description").text(localStorage.getItem("hour-10"));
-  $("#hour-11 .description").text(localStorage.getItem("hour-11"));
-  $("#hour-12 .description").text(localStorage.getItem("hour-12"));
-  $("#hour-13 .description").text(localStorage.getItem("hour-13"));
-  $("#hour-14 .description").text(localStorage.getItem("hour-14"));
-  $("#hour-15 .description").text(localStorage.getItem("hour-15"));
-  $("#hour-16 .description").text(localStorage.getItem("hour-16"));
-  $("#hour-17 .description").text(localStorage.getItem("hour-17"));
-
-  function timeClock() {
-    var currentTime = dayjs().hour();
-    //for each time block, the id of each div is separated by the "hour-" and the number to
-    //allow the comparison to the actual time
-    $(".time-block").each(function () {
-      var hour = parseInt($(this).attr("id").split("hour-")[1]);
-      console.log(currentTime, hour);
-      if (hour < currentTime) {
-        $(this).addClass("past");
-        $(this).removeClass("future");
-        $(this).removeClass("present");
-      } else if (hour === currentTime) {
-        $(this).removeClass("past");
-        $(this).addClass("present");
-        $(this).removeClass("future");
-      } else {
-        $(this).removeClass("present");
-        $(this).removeClass("past");
-        $(this).addClass("future");
-      }
+      // places items in local storage
+      localStorage.setItem(timeSelector, inputText);
     })
+    // user input saved in localStorage set to the values of the individual textarea elements.
+    $("#hour-9 .description").text(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").text(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").text(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").text(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").text(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").text(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").text(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").text(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").text(localStorage.getItem("hour-17"));
 
-  }
-  timeClock();
- 
-  //displays the current date and time
-  setInterval(function () {
-    var today = dayjs();
-    $("#currentDay").text(today.format("MMM D, YYYY, h:mm:ss"));
-  }, 1000)
+    function timeClock() {
+      var currentTime = dayjs().hour();
+      //for each time block, the id of each div is separated by the "hour-" and the number to
+      //allow the comparison to the actual time
+      $(".time-block").each(function () {
+        var hour = parseInt($(this).attr("id").split("hour-")[1]);
+        console.log(currentTime, hour);
+        if (hour < currentTime) {
+          $(this).addClass("past");
+          $(this).removeClass("future");
+          $(this).removeClass("present");
+        } else if (hour === currentTime) {
+          $(this).removeClass("past");
+          $(this).addClass("present");
+          $(this).removeClass("future");
+        } else {
+          $(this).removeClass("present");
+          $(this).removeClass("past");
+          $(this).addClass("future");
+        }
+      })
+
+    }
+    timeClock();
+
+    //displays the current date and time
+    setInterval(function () {
+      var today = dayjs();
+      $("#currentDay").text(today.format("MMM D, YYYY, h:mm:ss"));
+    }, 1000)
+  });
 });
